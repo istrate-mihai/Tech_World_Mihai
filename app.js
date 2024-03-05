99bdb50682d2f6290b18e524ec13426261c22298
Istrate Mihai Septimius <i.mihai9960@gmail.com>
2024-03-05 22:48:12 +0200

    Refactored to Jquery and fix mobile icon display on white theme change

--- app.js
+++ app.js
@@ -1,215 +1,203 @@
 import { projects, skills, educations, works } from "./data.js";
 
-const sections = document.querySelectorAll(".section");
-const sectBtns = document.querySelectorAll(".controlls");
-const sectBtn = document.querySelectorAll(".control");
-const allSections = document.querySelector(".main-content");
+$(document).ready(function() {
+    const sections    = $(".section");
+    const sectBtns    = $(".controlls");
+    const sectBtn     = $(".control");
+    const allSections = $(".main-content");
 
-let isLightMode = false;
+    let isLightMode   = false;
+    function setMainPhotoPopup() {
+      let modal       = $("#mainPhotoModal");
+      let img         = $("#main-photo");
+      let modalImg    = $("#modal-image");
+      let captionText = $("#caption");
+      let sectBtns    = $(".controlls");
 
-function setMainPhotoPopup() {
-  // Get the modal
-  let modal = document.getElementById("mainPhotoModal");
-
-  // Get the image and insert it inside the modal - use its "alt" text as a caption
-  let img = document.getElementById("main-photo");
-  let modalImg = document.getElementById("modal-image");
-  let captionText = document.getElementById("caption");
-
-  img.onclick = function() {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = '';
-  }
-
-  // Get the <span> element that closes the modal
-  let span = document.getElementsByClassName("close")[0];
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
-}
-
-function setPageTransitions() {
-  //Button click active class
-  for (let i = 0; i < sectBtn.length; i++) {
-    sectBtn[i].addEventListener("click", function () {
-      let currentBtn = document.querySelectorAll(".active-btn");
-      currentBtn[0].className = currentBtn[0].className.replace(
-        "active-btn",
-        ""
-      );
-      this.className += " active-btn";
-    });
-  }
-  //Sections Active
-  allSections.addEventListener("click", (e) => {
-    const id = e.target.dataset.id;
-    if (id) {
-      //remove selected from the other btns
-      sectBtns.forEach((btn) => {
-        btn.classList.remove("active");
+      img.click(function() {
+          modal.css("display", "block");
+          modalImg.attr("src", this.src);
+          captionText.html('');
+          sectBtns.css("display", "none");
       });
-      e.target.classList.add("active");
-      //hide other sections
-      sections.forEach((section) => {
-        section.classList.remove("active");
+
+      let span = $(".close").eq(0);
+
+      span.click(function() {
+          modal.css("display", "none");
+          sectBtns.css("display", "flex");
       });
-      const element = document.getElementById(id);
-      element.classList.add("active");
     }
-  });
 
-  //Toggle theme
-  const themeBtn = document.querySelector(".theme-btn");
-  themeBtn.addEventListener("click", () => {
-    let element = document.body;
-    element.classList.toggle("light-mode");
-    isLightMode = !isLightMode;
+    function setPageTransitions() {
+      //Button click active class
+      sectBtn.click(function() {
+          let currentBtn = $(".active-btn");
+          currentBtn.removeClass("active-btn");
+          $(this).addClass("active-btn");
+      });
 
-    if (isLightMode) {
-      themeBtn.setAttribute('title', 'Dark Mode');
+      //Sections Active
+      allSections.on("click", (e) => {
+          const id = $(e.target).data("id");
+          if (id) {
+              //remove selected from the other btns
+              sectBtns.removeClass("active");
+              $(e.target).addClass("active");
+              //hide other sections
+              sections.removeClass("active");
+              $("#" + id).addClass("active");
+          }
+      });
+
+      //Toggle theme
+      const themeBtn = $(".theme-btn");
+      themeBtn.click(function() {
+          $("body").toggleClass("light-mode");
+          isLightMode = !isLightMode;
+
+          if (isLightMode) {
+              $(this).attr('title', 'Dark Mode');
+          } else {
+              $(this).attr('title', 'Light Mode');
+          }
+      });
+  }
+
+    setPageTransitions();
+    setMainPhotoPopup();
+
+    let projectsArea       = $(".portfolios").eq(0);
+    let nrOfProjects       = projects.length;
+    let projectsInserted   = "";
+    let skillsArea         = $(".progress-bars").eq(0);
+    let nrOfSkills         = skills.length;
+    let skillsInserted     = "";
+    let educationsArea     = $(".timeline").eq(1);
+    let nrOfEducations     = educations.length;
+    let educationsInserted = "";
+    let worksArea          = $(".timeline").eq(2);
+    let nrOfWorks          = works.length;
+    let worksInserted      = "";
+
+    for (let i = 0; i < nrOfProjects; i++) {
+      projectsInserted += `
+          <div class="portfolio-item">
+            <h2 class="item-header">${projects[i].name}</h2>
+            <div class="image">
+              <img src="./images/${projects[i].img}" alt="portfolioImage" />
+            </div>
+            <div class="hover-items">
+              <h3>Project Source</h3>
+              <div class="icons">
+                <a href="${projects[i].github}" target="_blank" class="icon">
+                  <i class="fab fa-github"></i>
+                </a>
+                <a href="${projects[i].website}" target="_blank" class="icon">
+                  <i class='fas fa-desktop'></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        `;
     }
-    else {
-      themeBtn.setAttribute('title', 'Light Mode');
+
+    for (let i = 0; i < nrOfSkills; i++) {
+      skillsInserted += `
+        <div class="progress-bar">
+          <p class="prog-title">${skills[i].skillTitle}</p>
+          <div class="progress-con">
+            <p class="prog text">${skills[i].skillPercentage}</p>
+            <div class="progress">
+              <span class="${skills[i].cls}"></span>
+            </div>
+          </div>
+        </div>
+      `;
     }
-  });
-}
 
-setPageTransitions();
-setMainPhotoPopup();
-
-let projectsArea = document.getElementsByClassName("portfolios")[0];
-let nrOfProjects = projects.length;
-let projectsInserted = "";
-let skillsArea = document.getElementsByClassName("progress-bars")[0];
-let nrOfSkills = skills.length;
-let skillsInserted = "";
-let educationsArea = document.getElementsByClassName("timeline")[1];
-let nrOfEducations = educations.length;
-let educationsInserted = "";
-let worksArea = document.getElementsByClassName("timeline")[2];
-let nrOfWorks = works.length;
-let worksInserted = "";
-
-for (let i = 0; i < nrOfProjects; i++) {
-  projectsInserted += `
-                        <div class="portfolio-item">
-                          <h2 class="item-header">${projects[i].name}</h2>
-                          <div class="image">
-                            <img src="./images/${projects[i].img}" alt="portfolioImage" />
-                          </div>
-                          <div class="hover-items">
-                            <h3>Project Source</h3>
-                            <div class="icons">
-                              <a href="${projects[i].github}" target="_blank" class="icon">
-                                <i class="fab fa-github"></i>
-                              </a>
-                              <a href="${projects[i].website}" target="_blank" class="icon">
-                                <i class='fas fa-desktop'></i>
-                              </a>
+    for (let i = 0; i < nrOfEducations - 1; i++) {
+      educationsInserted += `
+                            <div class="timeline-item">
+                              <div class="tl-icon">
+                                <i class="fa fa-briefcase"></i>
+                              </div>
+                              <p class="tl-duration">${educations[i].duration}</p>
+                              <h5>
+                                ${educations[i].educationTitle}
+                                <span>
+                                  -
+                                  <a href="${educations[i].link}" target="blank">
+                                    ${educations[i].institutionTitle}
+                                  </a>
+                                </span>
+                              </h5>
+                              <p>${educations[i].institutionDescription}</p>
                             </div>
-                          </div>
-                        </div>
-                      `;
-}
-
-for (let i = 0; i < nrOfSkills; i++) {
-  skillsInserted += `
-                      <div class="progress-bar">
-                        <p class="prog-title">${skills[i].skillTitle}</p>
-                        <div class="progress-con">
-                          <p class="prog text">${skills[i].skillPercentage}</p>
-                          <div class="progress">
-                            <span class="${skills[i].cls}"></span>
-                          </div>
-                        </div>
-                      </div>
-                    `;
-}
-
-for (let i = 0; i < nrOfEducations - 1; i++) {
-  educationsInserted += `
-                          <div class="timeline-item">
-                            <div class="tl-icon">
-                              <i class="fa fa-briefcase"></i>
-                            </div>
-                            <p class="tl-duration">${educations[i].duration}</p>
-                            <h5>
-                              ${educations[i].educationTitle}
-                              <span>
-                                -
-                                <a href="${educations[i].link}" target="blank">
-                                  ${educations[i].institutionTitle}
-                                </a>
-                              </span>
-                            </h5>
-                            <p>${educations[i].institutionDescription}</p>
-                          </div>
-                        `;
-}
-
-let otherEducationLinks = "";
-let nrOfotherEducationLinks =
-  educations[nrOfEducations - 1].educationTitle.length; /*
-                                                          Accessing the last object of educations array to get
-                                                          all the links of it's educationTitle property
-                                                        */
-for (let i = 0; i < nrOfotherEducationLinks; i++) {
-  otherEducationLinks += `
-                            <p class="otherCertificate">
-                              <a href="${
-                                educations[nrOfEducations - 1].educationTitle[i]
-                                  .otherEducationLink
-                              }" target="_blank">
-                              ${
-                                educations[nrOfEducations - 1].educationTitle[i]
-                                  .otherEducationTitle
-                              }
-                              </a>
-                            </p>
                           `;
-}
+    }
 
-educationsInserted += `
-                        <div class="timeline-item">
-                          <div class="tl-icon">
-                            <i class="fa fa-briefcase"></i>
-                          </div>
-                          <p class="tl-duration">Other Certificates</p>
-                          ${otherEducationLinks}
-                        </div>
+    let otherEducationLinks = "";
+    let nrOfotherEducationLinks =
+      educations[nrOfEducations - 1].educationTitle.length; /*
+                                                              Accessing the last object of educations array to get
+                                                              all the links of it's educationTitle property
+                                                            */
+    for (let i = 0; i < nrOfotherEducationLinks; i++) {
+      otherEducationLinks += `
+                          <p class="otherCertificate">
+                            <a href="${
+      educations[nrOfEducations - 1].educationTitle[i]
+          .otherEducationLink
+      }" target="_blank">
+                            ${
+      educations[nrOfEducations - 1].educationTitle[i]
+          .otherEducationTitle
+      }
+                            </a>
+                          </p>
                         `;
+    }
 
-for (let i = 0; i < nrOfWorks; i++) {
-  // Getting all the elements of the responsabilities array property for each work object and inserting them in the template
-  let workListItems = "";
-  let responsabilities = works[i].responsabilities;
-  let nrOfWorkListItems = responsabilities.length;
+    educationsInserted += `
+      <div class="timeline-item">
+        <div class="tl-icon">
+          <i class="fa fa-briefcase"></i>
+        </div>
+        <p class="tl-duration">Other Certificates</p>
+        ${otherEducationLinks}
+      </div>
+      `;
 
-  for (let j = 0; j < nrOfWorkListItems; j++) {
-    workListItems += `<li>${responsabilities[j]}</li>`;
-  }
+    for (let i = 0; i < nrOfWorks; i++) {
+      // Getting all the elements of the responsabilities array property for each work object and inserting them in the template
+      let workListItems     = "";
+      let responsabilities  = works[i].responsabilities;
+      let nrOfWorkListItems = responsabilities.length;
 
-  worksInserted += `
-                    <div class="timeline-item">
-                      <div class="tl-icon">
-                        <i class="fa fa-briefcase"></i>
-                      </div>
-                      <p class="tl-duration">${works[i].duration}</p>
-                      <h5>${works[i].title}</h5>
-                      <p>Responsibilities:</p>
-                      <p>
-                        <ul>
-                          ${workListItems}
-                        </ul>
-                      </p>
-                    </div>
-                  `;
-}
+      for (let j = 0; j < nrOfWorkListItems; j++) {
+          workListItems += `<li>${responsabilities[j]}</li>`;
+      }
 
-projectsArea.innerHTML = projectsInserted;
-skillsArea.innerHTML = skillsInserted;
-educationsArea.innerHTML = educationsInserted;
-worksArea.innerHTML = worksInserted;
+      worksInserted += `
+          <div class="timeline-item">
+            <div class="tl-icon">
+              <i class="fa fa-briefcase"></i>
+            </div>
+            <p class="tl-duration">${works[i].duration}</p>
+            <h5>${works[i].title}</h5>
+            <p>Responsibilities:</p>
+            <p>
+              <ul>
+                ${workListItems}
+              </ul>
+            </p>
+          </div>
+        `;
+    }
+
+    projectsArea.html(projectsInserted);
+    skillsArea.html(skillsInserted);
+    educationsArea.html(educationsInserted);
+    worksArea.html(worksInserted);
+});
