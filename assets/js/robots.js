d91f567910d68aaae53325c1449137176387c447
Istrate-Mihai <i.mihai9960@gmail.com>
2024-11-24 15:36:42 +0200

    Updated lazy loading implementation

--- assets/js/robots.js
+++ assets/js/robots.js
@@ -1,19 +1,17 @@
 $(document).ready(() => {
   $('.logo-container').css('background-image', 'url(../../assets/images/robots-collection.jpg)');
-  let videoList = $('video');
-  let config    = {
-    rootMargin: '0px 0px 200px 0px',
-    threshold: 0
-  };
 
-  let observer = new IntersectionObserver((entries, self) => {
-    entries.forEach(entry => {
-      let video       = $(entry.target);
-      video.attr('src', video.data('src'));
-      video.removeAttr('data-src');
-      self.unobserve(entry.target);
+  let videoList = $('video');
+  let observer  = new IntersectionObserver((entries, self) => {
+    $(entries).each((index, entry) => {
+      if (entry.isIntersecting) {
+        let video = $(entry.target);
+        video.attr('src', video.data('src'));
+        video.removeAttr('data-src');
+        self.unobserve(entry.target);
+      }
     });
-  }, config);
+  });
 
   videoList.each((index, video) => {
     observer.observe(video);
